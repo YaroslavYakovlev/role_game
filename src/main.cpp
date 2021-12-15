519d6932de3c413ce647a2f77c267c87c74c8be9
Yaroslav Yakovlev <ekb-buza@mail.ru>
2021-12-15 21:22:32 +0500

    Попытка раставить юнитов

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,16 @@
 #include <ctime>
 #include <windows.h>
 
+// #define HERO
+// #define PRINT
+
 const int col = 10;
 const int row = 10; 
 char arena[col][row];
+int X = -1;
+int Y = -1;
+bool bEnumy = false;
+bool bHero = false;
 
 struct Person {
   std::string namePerson = "Unknown";
@@ -14,13 +21,19 @@ struct Person {
   int damagePerson = 0;
 };
 
-void creating_enemies(std::vector<Person>& p, int countEnumy){
-  for (int i = 0; i < countEnumy; i++) {
+void coordinates(){
+  X = rand() % 11;
+  Y = rand() % 11;
+  std::cout << "X - " << X << " Y - " << Y << std::endl;
+}
+
+void creating_enemies(std::vector<Person>& p, int i){
     p[i].namePerson = ("Enumy #" + std::to_string(i+1));
     p[i].hpPerson = rand() % 100 + 50;
     p[i].armorPerson = rand() % 51;
     p[i].damagePerson = rand() % 15 + 15;
-  }
+    // coordinates();
+    bEnumy = true;
 }
 
 void takeDamage(Person& p, int damage_received){
@@ -31,14 +44,22 @@ void takeDamage(Person& p, int damage_received){
   }
 }
 
-void field(char (& _arena), int _col, int _row){
+void field(char _arena[col][row], int _X, int _Y){
 
     // int columns = sizeof(field[0]) / sizeof(field[0][0]);
     std::cout << std::endl;
-    for(int i = 0; i < _col; ++i){
-        for(int j = 0; j < _row; ++j){
-          _arena[i][j];
-            // std::cout << "." << " ";
+    for(int i = 0; i < col; ++i){
+        for(int j = 0; j < row; ++j){
+          _arena[i][j] = '.';
+          if(bEnumy){
+            _arena[_X][_Y] = 'E';
+            // bEnumy = false;
+            }
+          else if(bHero){
+            _arena[_X][_Y] = 'P';
+            // bHero = false;
+          }
+            std::cout << _arena[i][j] << " ";
         }
         std::cout << std::endl;
     }
@@ -49,22 +70,30 @@ int main(){
   const int countEnumy = 5;
   std::vector<Person> pers(countEnumy);
   Person hero;
-  int damagePerson = 0;
 
   srand(time(NULL));
+  #ifdef HERO
   std::cout << "Enter name, HP, armor, damage your person" << std::endl;
   std::cin >> hero.namePerson;
   std::cin >> hero.hpPerson;
   std::cin >> hero.armorPerson;
   std::cin >> hero.damagePerson;
+  #endif 
+  bHero = true;
+  coordinates();
 
-  creating_enemies(pers, countEnumy);
+  for (int i = 0; i < countEnumy; i++) {
+    creating_enemies(pers, i);
+    coordinates();
+  std::cout << "X = " << X << std::endl;
+  std::cout << "Y = " << Y << std::endl;
+  }
+  field(arena, X, Y);
 
-  field(arena, col, row);
   // takeDamage(pers, damagePeron);
 
 
-
+#ifdef PRINT
   for(int i = 0; i < countEnumy; i++) {
     std::cout << "Name - " << pers[i].namePerson << std::endl;
     std::cout << "HP - " << pers[i].hpPerson << std::endl;
@@ -78,6 +107,6 @@ int main(){
     std::cout << "Armor - " << hero.armorPerson << std::endl;
     std::cout << "Damage - " << hero.damagePerson << std::endl;
     std::cout << std::endl;
-
+#endif 
   return 0;
 }

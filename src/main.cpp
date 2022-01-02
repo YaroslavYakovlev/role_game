47e81b7dde691eee77fb0d436023cb7160790478
Yaroslav Yakovlev <ekb-buza@mail.ru>
2022-01-02 21:57:33 +0500

    есть передвижение врагов

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,17 @@
+#include <ctime>
 #include <iostream>
 #include <vector>
-#include <ctime>
 
 #define HERO
 // #define PRINT_HERO
 // #define PRINT_ENUMY
 
 const int col = 10;
-const int row = 10; 
+const int row = 10;
 const int countEnumy = 5;
 char arena[col][row];
 std::string steps[] = {"left", "right", "top", "bottom"};
-std::string word[countEnumy] = "";
+std::string word = "";
 bool bFlag = false;
 
 struct Person {
@@ -24,74 +24,64 @@ struct Person {
 };
 
 struct Steps {
-  std::string left = "left";        // Лево
-  std::string right = "right";      // Право 
-  std::string top = "top";          // Вверх
-  std::string bottom  = "bottom";   // Низ
+  std::string left = "left";      // Лево
+  std::string right = "right";    // Право
+  std::string top = "top";        // Вверх
+  std::string bottom = "bottom";  // Низ
 };
 
-// void coordinates(std::vector<Person>& p){
-//   p.X = rand() % 11;
-//   Y = rand() % 11;
-//   std::cout << "X - " << X << " Y - " << Y << std::endl;
-// }
-
-void creating_enemies(std::vector<Person>& p, int i){
-    p[i].namePerson = ("Enumy #" + std::to_string(i+1));
-    p[i].hpPerson = rand() % 100 + 50;
-    p[i].armorPerson = rand() % 51;
-    p[i].damagePerson = rand() % 15 + 15;
-    p[i].X = rand() % 10 + 1;
-    p[i].Y = rand() % 10 + 1;
+void creating_enemies(std::vector<Person>& p, int i) {
+  p[i].namePerson = ("Enumy #" + std::to_string(i + 1));
+  p[i].hpPerson = rand() % 100 + 50;
+  p[i].armorPerson = rand() % 51;
+  p[i].damagePerson = rand() % 15 + 15;
+  p[i].X = rand() % 10 + 1;
+  p[i].Y = rand() % 10 + 1;
 }
 
-void takeDamage(Person& p, int damage_received){
+void takeDamage(Person& p, int damage_received) {
   p.armorPerson -= damage_received;
-  if(p.armorPerson < 0){
+  if (p.armorPerson < 0) {
     p.hpPerson += p.armorPerson;
     p.armorPerson = 0;
   }
 }
 
-void field(char _arena[row][col], std::vector<Person>& p, Person& h){
-    std::cout << std::endl;
-      for(int i = 1; i <= row; ++i){
-          for(int j = 1; j <= col; ++j){
-            _arena[i][j] = '.';
-              for(int k = 0; k < p.size(); k++){
-                if(bFlag){
-                  word[k] = steps[rand() % 4];
-                }
-                _arena[p[k].X][p[k].Y] = 'E';
-                if(!word[k].empty()){
-                  if(word[k] == "top"){
-                      p[k].X -= 1;
-                  }else if(word[k] == "bottom"){
-                      p[k].X += 1;
-                  }else if(word[k] == "left"){
-                      p[k].Y -= 1;
-                  }else if(word[k] == "right"){
-                      p[k].Y += 1;
-                  }
-                }
-                if(h.X <= row || h.Y <= col)
-                  _arena[h.X][h.Y] = 'P';
-                // if((h.X == p[k].X) && (h.Y == p[k].Y)){
-                //   std::cout << "GOOD" << std::endl;
-                // }
-              }
-              std::cout << _arena[i][j] << " ";
-          }
-          std::cout << std::endl;
+void field(char _arena[row][col], std::vector<Person>& p, Person& h) {
+  std::cout << std::endl;
+  for (int i = 1; i <= row; ++i) {
+    for (int j = 1; j <= col; ++j) {
+      _arena[i][j] = '.';
+      for (int k = 0; k < p.size(); k++) {
+        _arena[p[k].X][p[k].Y] = 'E';
+        if (h.X <= row || h.Y <= col)
+          _arena[h.X][h.Y] = 'P';
+        // if((h.X == p[k].X) && (h.Y == p[k].Y)){
+        //   std::cout << "GOOD" << std::endl;
+        // }
       }
-      for(int f = 0; f < p.size(); f++){
-
-        std::cout << "Word - " << word[f] << std::endl;
-      }
+      std::cout << _arena[i][j] << " ";
+    }
     std::cout << std::endl;
+  }
+  std::cout << std::endl;
 }
 
-int main(){
+void enemy_move(std::vector<Person>& p, int i) {
+  word = steps[rand() % 4];
+  if (word == "top") {
+    p[i].X -= 1;
+  } else if (word == "bottom") {
+    p[i].X += 1;
+  } else if (word == "left") {
+    p[i].Y -= 1;
+  } else if (word == "right") {
+    p[i].Y += 1;
+  }
+  std::cout << "Word - " << word << std::endl;
+}
+
+int main() {
   std::cout << "Role game" << std::endl;
   std::string stepHero;
   std::vector<Person> pers(countEnumy);
@@ -99,7 +89,7 @@ int main(){
   Steps step;
 
   srand(time(NULL));
-  #ifdef HERO
+#ifdef HERO
   std::cout << "Enter name, HP, armor, damage your person" << std::endl;
   std::cin >> hero.namePerson;
   std::cin >> hero.hpPerson;
@@ -107,32 +97,32 @@ int main(){
   std::cin >> hero.damagePerson;
   hero.X = rand() % 10 + 1;
   hero.Y = rand() % 10 + 1;
-  #endif 
+#endif
   for (int i = 0; i < countEnumy; i++) {
     creating_enemies(pers, i);
   }
   field(arena, pers, hero);
 
   // takeDamage(pers, 10);
-  while(countEnumy > 0 || hero.hpPerson > 0){
-    std::cout << "The hero walks" << std::endl; 
+  while (countEnumy > 0 || hero.hpPerson > 0) {
+    std::cout << "The hero walks" << std::endl;
     std::cin >> stepHero;
-    if(stepHero == step.top){
-      hero.X -= 1; 
-    }else if(stepHero == step.bottom){
+    if (stepHero == step.top) {
+      hero.X -= 1;
+    } else if (stepHero == step.bottom) {
       hero.X += 1;
-    }else if(stepHero == step.left){
+    } else if (stepHero == step.left) {
       hero.Y -= 1;
-    }else if(stepHero == step.right){
+    } else if (stepHero == step.right) {
       hero.Y += 1;
     }
-    // word = steps[rand() % 4];
-    bFlag = true;
+    for (int i = 0; i < countEnumy; i++)
+      enemy_move(pers, i);
     field(arena, pers, hero);
   }
 
 #ifdef PRINT_ENUMY
-  for(int i = 0; i < countEnumy; i++) {
+  for (int i = 0; i < countEnumy; i++) {
     std::cout << "Name - " << pers[i].namePerson << std::endl;
     std::cout << "HP - " << pers[i].hpPerson << std::endl;
     std::cout << "Armor - " << pers[i].armorPerson << std::endl;
@@ -143,14 +133,14 @@ int main(){
     std::cout << std::endl;
   }
 #endif
-#ifdef  PRINT_HERO
-    std::cout << "Name - " << hero.namePerson << std::endl;
-    std::cout << "HP - " << hero.hpPerson << std::endl;
-    std::cout << "Armor - " << hero.armorPerson << std::endl;
-    std::cout << "Damage - " << hero.damagePerson << std::endl;
-    std::cout << "Coordinats X - " << hero.X << std::endl;
-    std::cout << "Coordinats Y - " << hero.Y << std::endl;
-    std::cout << std::endl;
-#endif 
+#ifdef PRINT_HERO
+  std::cout << "Name - " << hero.namePerson << std::endl;
+  std::cout << "HP - " << hero.hpPerson << std::endl;
+  std::cout << "Armor - " << hero.armorPerson << std::endl;
+  std::cout << "Damage - " << hero.damagePerson << std::endl;
+  std::cout << "Coordinats X - " << hero.X << std::endl;
+  std::cout << "Coordinats Y - " << hero.Y << std::endl;
+  std::cout << std::endl;
+#endif
   return 0;
 }

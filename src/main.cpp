746a605067de184c86ef69b0c760dac6af1906f2
Yaroslav Yakovlev <ekb-buza@mail.ru>
2022-01-03 18:15:14 +0500

    save and load

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <ctime>
+#include <fstream>
 #include <iostream>
 #include <vector>
 
@@ -12,7 +13,7 @@ const int countEnumy = 5;
 char arena[col][row];
 std::string steps[] = {"left", "right", "top", "bottom"};
 std::string word = "";
-bool bFlag = false;
+bool bConflict = false;
 
 struct Person {
   std::string namePerson = "Unknown";
@@ -47,6 +48,10 @@ void takeDamage(Person& p, int damage_received) {
   }
 }
 
+void conflict() {
+  std::cout << "GOOD" << std::endl;
+}
+
 void field(char _arena[row][col], std::vector<Person>& p, Person& h) {
   std::cout << std::endl;
   for (int i = 1; i <= row; ++i) {
@@ -56,9 +61,9 @@ void field(char _arena[row][col], std::vector<Person>& p, Person& h) {
         _arena[p[k].X][p[k].Y] = 'E';
         if (h.X <= row || h.Y <= col)
           _arena[h.X][h.Y] = 'P';
-        // if((h.X == p[k].X) && (h.Y == p[k].Y)){
-        //   std::cout << "GOOD" << std::endl;
-        // }
+        if ((h.X == p[k].X) && (h.Y == p[k].Y)) {
+          bConflict = true;
+        }
       }
       std::cout << _arena[i][j] << " ";
     }
@@ -81,12 +86,51 @@ void enemy_move(std::vector<Person>& p, int i) {
   std::cout << "Word - " << word << std::endl;
 }
 
+void save_progress(std::ofstream& file, Person& p, Person& h) {
+  int lenP = p.namePerson.length();
+  int lenH = h.namePerson.length();
+
+  file.write((char*)&lenP, sizeof(lenP));
+  file.write((char*)&lenH, sizeof(lenH));
+
+  file.write(p.namePerson.c_str(), lenP);
+  file.write(h.namePerson.c_str(), lenH);
+
+  file.write((char*)&p.hpPerson, sizeof(p.hpPerson));
+  file.write((char*)&h.hpPerson, sizeof(h.hpPerson));
+
+  file.write((char*)&p.armorPerson, sizeof(p.armorPerson));
+  file.write((char*)&h.armorPerson, sizeof(h.armorPerson));
+}
+
+void load_progress(std::ifstream& file, Person& p, Person& h) {
+  int lenP;
+  int lenH;
+
+  file.read((char*)&lenP, sizeof(lenP));
+  file.read((char*)&lenH, sizeof(lenH));
+
+  p.namePerson.resize(lenP);
+  h.namePerson.resize(lenH);
+
+  file.read((char*)p.namePerson.c_str(), lenP);
+  file.read((char*)h.namePerson.c_str(), lenH);
+
+  file.read((char*)&p.hpPerson, sizeof(p.hpPerson));
+  file.read((char*)&h.hpPerson, sizeof(h.hpPerson));
+
+  file.read((char*)&p.armorPerson, sizeof(p.armorPerson));
+  file.read((char*)&h.armorPerson, sizeof(h.armorPerson));
+}
+
 int main() {
   std::cout << "Role game" << std::endl;
   std::string stepHero;
   std::vector<Person> pers(countEnumy);
   Person hero;
   Steps step;
+  std::ofstream save("..\\src\\File\\save.bin", std::ios::binary);
+  std::ifstream load("..\\src\\File\\save.bin", std::ios::binary);
 
   srand(time(NULL));
 #ifdef HERO
@@ -107,7 +151,15 @@ int main() {
   while (countEnumy > 0 || hero.hpPerson > 0) {
     std::cout << "The hero walks" << std::endl;
     std::cin >> stepHero;
-    if (stepHero == step.top) {
+    if (stepHero == "save")
+      for (int i = 0; i < countEnumy; i++) {
+        save_progress(save, pers[i], hero);
+      }
+    else if (stepHero == "load")
+      for (int i = 0; i < countEnumy; i++) {
+        load_progress(load, pers[i], hero);
+      }
+    else if (stepHero == step.top) {
       hero.X -= 1;
     } else if (stepHero == step.bottom) {
       hero.X += 1;
@@ -116,9 +168,14 @@ int main() {
     } else if (stepHero == step.right) {
       hero.Y += 1;
     }
+    /**
+     * * Закоментировал рабочий код (передвижение врагов)
     for (int i = 0; i < countEnumy; i++)
       enemy_move(pers, i);
+     */
     field(arena, pers, hero);
+    if (bConflict)
+      conflict();
   }
 
 #ifdef PRINT_ENUMY
